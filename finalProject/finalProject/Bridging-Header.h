8875d35872a938d271bda19262b3d5678d50af9c
Corrin <corri108@llb12-imac-05.chapman.edu>
2016-05-09 20:14:53 -0700

    Added Facebook Functionality

---
+++ finalProject/finalProject/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  finalProject
+//
+//  Created by Corrin, Will on 5/9/16.
+//  Copyright © 2016 Corrin, Will. All rights reserved.
+//
+
+#ifndef FacebookTutorial_Bridging_Header_h
+#define FacebookTutorial_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif
